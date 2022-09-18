c29c634f74931c873a6787fdbd5f1500ee2176c2
Nishchaiya Bayla <nishchaiya2001@gamil.com>
2022-09-18 16:05:01 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2f8cabe1a4msh80763738b57fd48p1623ebjsna6ff40da6f8f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7f0a6031bf460b09f2a42ac56231932c";
